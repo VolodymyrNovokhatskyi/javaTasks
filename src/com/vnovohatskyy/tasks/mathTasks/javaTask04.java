5659b70975c8092dae9ec4d716059f8cc6939d55
VolodymyrNovokhatskyi <vnovohatskyy@yahoo.com>
2018-03-25 14:11:53 +0300

    added math and string tasks

---
+++ src/com/vnovohatskyy/tasks/mathTasks/javaTask04.java
@@ -0,0 +1,13 @@
+package com.vnovohatskyy.tasks.mathTasks;
+
+public class javaTask04 {
+    public static void main(String[] args) {
+        double n4=23.40;
+        if (n4%(int)(n4)>=0.5){
+            System.out.println((int)n4+1);
+        } else {
+            System.out.println((int)n4);
+        }
+
+    }
+}
